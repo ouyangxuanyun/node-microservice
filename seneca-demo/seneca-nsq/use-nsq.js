7713a7b67b598a0ac4b7f361bc91f0cefcd7fe0e
fu <youtianzhan@126.com>
2018-08-01 17:14:06 +0800

    feat: test nsq (fail)

--- seneca-demo/seneca-nsq/use-nsq.js
+++ seneca-demo/seneca-nsq/use-nsq.js
@@ -1,16 +1,17 @@
 const seneca = require('seneca')();
-// require('seneca')()
-//   .use('nsq-transport')
-//   .add('foo:two',function(args,done){ done(null,{bar:args.bar}) })
-//   // .client( {type:'nsq',pin:'foo:one,bar:*'} )
-//   // .listen( {type:'nsq',pin:'foo:two,bar:*'} )
-//   .listen( {type:'nsq',pin:'foo:two,bar:*'} )
-
 
 seneca
-  .use('color')
-  .listen({type:'tcp'})
+  .use('nsq-transport')
+  .add('foo:two',function(args,done){ done(null,{bar:args.bar}) })
+  // .client( {type:'nsq',pin:'foo:one,bar:*'} )
+  // .listen( {type:'nsq',pin:'foo:two,bar:*'} )
+  .listen( {type:'nsq',pin:'foo:two,bar:*'} )
 
-seneca
-  .client({type:'tcp'})
-  .act('color:red')
+
+// seneca
+//   .use('color')
+//   .listen({type:'tcp'})
+//
+// seneca
+//   .client({type:'tcp'})
+//   .act('color:red')
